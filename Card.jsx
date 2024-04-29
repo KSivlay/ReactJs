3bbc132727ec6d3ab7eb2c16086beb273655706c
KSivlay <168314689+KSivlay@users.noreply.github.com>
2024-04-29 14:01:39 +0700

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Card.css"
+
+const CreateCard = () => {
+  return(
+    <>
+        <div className="card"> 
+            This is Components Folder.
+        </div>
+        <button className="clickme">Click me</button>
+    </>
+  );
+};
+
+export default CreateCard
+
